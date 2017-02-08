b1636ef64f4be9d706a90b7190ec1f09928e5ba6
shrishsinghal <singhalshrish@gmail.com>
2017-02-08 21:57:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(res,req){
+    res.send('Article one requested and will be served here');
+});
+
+app.get('/article-two', function(res,req){
+    res.send('Article two requested and will be served here');
+});
+
+app.get('/article-three', function(res,req){
+    res.send('Article three requested and will be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
