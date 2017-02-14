78901aa8bd3ff0fa0d4370c903c19ffbc56948fa
shrishsinghal <singhalshrish@gmail.com>
2017-02-14 21:02:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ function createTemplate (data) {
     return htmlTemplate ;
 }
 app.get('/', function (req, res) {
-res.sendFile(path.join(_dirname, 'ui', 'index.html'));
+res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 
