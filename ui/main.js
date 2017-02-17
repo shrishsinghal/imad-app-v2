72c41edfa7a261285864254cdd59814ec31079e3
shrishsinghal <singhalshrish@gmail.com>
2017-02-17 16:29:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,18 +16,21 @@ img.onclick = function () {
 // button
 var button = document.getElementById('counter');
 button.onclick = function () {
-    // Make a request to the counter endpoint
+    // Create a request object
     var request = new XMLHttpRequest();
     
     // Capture the response and store it in a variable.
     request.onreadystatechange = function () {
-        if(request.readyState === XMLHttprequest.DONE)
+        if(request.readyState === XMLHttprequest.DONE) {
         // Take some action
         if(request.status === 200) {
             var counter = request.responseText;
             var span = document.getElementById('counter');
             span.innerHTML = counter.toString();
         }
-        
+        }
     };
+    // MAke the request
+    request.open('GET', 'http://shrishsinghal.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
