8f9c2a6c44f94d70ae960305268b46d1fbfd67a3
shrishsinghal <singhalshrish@gmail.com>
2017-02-18 20:56:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,7 +14,7 @@ img.onclick = function () {
 
 
 // button
-var button = document.getElementById('count');
+var button = document.getElementById('counter');
 button.onclick = function () {
     // Create a request object
     var request = new XMLHttpRequest();
@@ -25,7 +25,7 @@ button.onclick = function () {
         // Take some action
         if(request.status === 200) {
             var counter = request.responseText;
-            var span = document.getElementById('counter');
+            var span = document.getElementById('count');
             span.innerHTML = counter.toString();
         }
         }
