8fff864fcd0b280b552f2d98019fc57e48a254cf
shrishsinghal <singhalshrish@gmail.com>
2017-02-20 21:35:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,3 +34,20 @@ button.onclick = function () {
     request.open('GET', 'http://shrishsinghal.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+// Submit name
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onclick = function () {
+    // make a request to the servaer and send the name
+    
+    // capture a list of names ans render it in a list
+    var names = ['name1', 'name2', 'name3'];
+    var list  = '';
+    for (var i=0; i < names.lenght; i++) {
+        list == '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
