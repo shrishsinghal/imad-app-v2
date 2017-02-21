df9dab04fb5f13e0266176076e988d36b8b23d3a
shrishsinghal <singhalshrish@gmail.com>
2017-02-21 21:21:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,8 +36,7 @@ button.onclick = function () {
 };
 
 // Submit name
-var nameInput = document.getElementById("name");
-var name = nameInput.value;
+
 var submit = document.getElementById("submit_btn");
 submit.onclick = function () {
     
@@ -62,6 +61,8 @@ submit.onclick = function () {
         }
     };
     // Make the request
+    var nameInput = document.getElementById("name");
+    var name = nameInput.value;
     request.open('GET', 'http://shrishsinghal.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
     
